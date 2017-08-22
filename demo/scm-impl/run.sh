0135012298053abcb2738f1744dcf4c1defa7066
Jesse Glick <jglick@cloudbees.com>
2017-08-22 15:23:51 -0400

    Basic functions implemented.

--- demo/scm-impl/run.sh
+++ demo/scm-impl/run.sh
@@ -9,13 +9,31 @@ case $COMMAND in (checkout)
     else
         r="$HEAD"
     fi
-    hg clone --updaterev "$r" "$CONFIG" . >&2
-    # TODO handle changelog
+    if [ -d .hg ]
+    then
+        hg pull --update --rev "$r" >&2
+    else
+        hg clone --updaterev "$r" "$CONFIG" . >&2
+    fi
+    if [ $CHANGELOG = true -a "$BASELINE" ]
+    then
+        hg log -r$(hg id -i)-"${BASELINE}" -T "{node|short}\n{author}\n${CONFIG}rev/{node|short}\n{sub(' .+','',date|hgdate)}\n{indent(desc, '> ')}\n{file_adds % '+ {file}\n'}{file_dels % '- {file}\n'}{file_mods % '* {file}\n'}\n"
+    fi
 ;; (identify)
-    # TODO
+    hg id -i | tr -d '\n'
 ;; (compare)
-    # TODO
-    echo SIGNIFICANT
+    if [ -z "$REVISION" ]
+    then
+        # or by hgweb: REVISION=$(wget -q -O - "${CONFIG}"branches | xmlstarlet sel -t -m "//_:tr[@class='tagEntry'][normalize-space(_:td[1]/_:a/text())='$HEAD']" -v 'normalize-space(_:td[2]/_:a)')
+        REVISION=$(hg id -r "$HEAD" "$CONFIG")
+    fi
+    if [ $REVISION = $BASELINE ]
+    then
+        echo NONE
+    else
+        # TODO not possible to differentiate without requiring workspace for polling, or doing a temporary clone
+        echo SIGNIFICANT
+    fi
     echo -n $REVISION
 ;; (*)
     echo Unknown command: $COMMAND >&2
